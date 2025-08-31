199b577cc873bbca440a37795e65075084a0b0c8
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2025-08-31 10:45:08 -0500

    Update Cognito configuration in CloudFormation and environment files to reflect new naming conventions for user pool and app client. Adjust oauthDomain to match the updated domain prefix for consistency across environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   cognito: {
     userPoolId: 'us-east-1_6DFewMzmv',
     clientId: '74h5hpn35aphlnvj5d1v2egb8h',
-    oauthDomain: 'angular-demo-gldiaz-1756643115.auth.us-east-1.amazoncognito.com',
+    oauthDomain: 'angular-cognito-amplify-2025.auth.us-east-1.amazoncognito.com',
     redirectSignIn: 'http://localhost:4200/',
     redirectSignOut: 'http://localhost:4200/',
     scope: 'openid email profile',
